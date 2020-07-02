673a6594732bee89cb67f1d8aa6d825d8120e21a
ReVa <58979786+sahandsame@users.noreply.github.com>
2020-07-02 23:21:10 +0300

    Update reva.js

--- reva.js
+++ reva.js
@@ -2,6 +2,21 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
 
+const init = async () => {
+  
+  
+  let fileR = await readdir("./commands/");
+  fileR.forEach(async (dir) => {
+    let commands = await readdir("./commands/"+dir+"/");
+    commands.filter((cmd) => cmd.split(".").pop() === "js").forEach((cmd) => {
+      const response = client.loadCommand("./commands/"+dir, cmd);
+      if(response){
+        client.logger.log(response, "error");
+        }
+      })
+    });
+                                                                    
+                                                                    
 
 
 client.login();
