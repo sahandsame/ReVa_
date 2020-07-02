fc2298b390dee159b076422c11a59bea8a161738
ReVa <58979786+sahandsame@users.noreply.github.com>
2020-07-02 23:51:35 +0300

    Create Command.js

---
+++ base/Command.js
@@ -0,0 +1,23 @@
+const path = require("path");
+module.exports = class Command {
+    constructor(client, {
+      name = null,
+      description = (language) => language.get("NO_DESCRIPTION_PROVIDED"),
+      usage = (language) => language.get("NO_USAGE_PROVIDED"),
+      examples = (language) => language.get("NO_EXAMPLE_PROVIDED"),
+      dirname = false,
+      enabled = true,
+      guildOnly = false,
+      aliases = new Array(),
+      botPermissions = new Array(),
+      memberPermissions = new Array(),
+      ownerOnly = false,
+      cooldown = 3000
+    })
+    {
+      let category = (dirname ? dirname.split(path.sep)[parseInt(dirname.split(path.sep).length-1, 10)] : "Other");
+      this.client = client;
+      this.conf = { enabled, guildOnly, aliases, memberPermissions, botPermissions, ownerOnly, cooldown};
+      this.help = { name, description, category, usage, examples };
+    }
+  };
