613e4c5916defa29aabb18101dede263af33ff84
ReVa <58979786+sahandsame@users.noreply.github.com>
2020-07-02 23:13:04 +0300

    Update reva.js

--- reva.js
+++ reva.js
@@ -2,5 +2,6 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
 
-let dataJ = JSON.parse(fs.readFileSync("./dataJ.json", "utf8"));
 
+
+client.login();
